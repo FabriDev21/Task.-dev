ceaf381d5e6c653b828563d689ac4821fc36339c
fabridev <fabridev45@gmail.com>
2023-11-03 20:10:37 -0500

    first-component-Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to ¡My to do list!</h1>
+
+        <p>A space for you, for your life, and your pending matters</p>
+
+        <img/>
+      </div>
+
+
+    </>
+  );
+}
+
+export default Header;
